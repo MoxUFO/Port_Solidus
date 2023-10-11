bfc765f02179a139ebd6a4c1306837312b2c1d43
MOX UFO <mox.ufo@gmail.com>
2023-10-11 16:59:34 -0400

    black background

--- src/componenets/PortfolioContainer.js
+++ src/componenets/PortfolioContainer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import NavBar from './NavBar'
+import '../assest/css/PortfolioContainer.css'
 
 
 export default function PortfolioContainer() {
   return (
-    <div>
+    <div className='port-container'>
         
         <NavBar />
     </div>
