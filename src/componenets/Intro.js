a3b7d6645ef000fea2bb45bbfc422d3d9b3a53dd
MOX UFO <mox.ufo@gmail.com>
2023-10-11 17:32:12 -0400

    background image set

---
+++ src/componenets/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assest/css/Intro.css'
+
+export default function Intro() {
+  return (
+    <div className='intro-bg'>
+        <div className='intro-img'></div>
+    </div>
+  )
+}
