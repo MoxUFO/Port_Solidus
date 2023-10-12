036d1f5f62bd017faf3bbeb9ffab35d274e2fd6a
MOX UFO <mox.ufo@gmail.com>
2023-10-12 15:29:09 -0400

    portfolio body reset

--- src/componenets/PortfolioContainer.js
+++ src/componenets/PortfolioContainer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import NavBar from './NavBar'
-import Intro from './Intro'
+
 import '../assest/css/PortfolioContainer.css'
 
 
 export default function PortfolioContainer() {
   return (
     <div className='port-container'>
-        < Intro />
+
         <NavBar />
     </div>
   )
