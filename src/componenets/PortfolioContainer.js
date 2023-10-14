558854654c3404f69206954592ef6948f6ef6a66
MOX UFO <mox.ufo@gmail.com>
2023-10-14 11:26:21 -0400

    refactor code so bg is set porperly and adjusted margin in intro

--- src/componenets/PortfolioContainer.js
+++ src/componenets/PortfolioContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from './NavBar'
-import Ikigai from './Ikigai'
+
 import Intro from './Intro'
 import ProjectContainer from './ProjectContainer'
 
@@ -10,7 +10,7 @@ import '../assest/css/PortfolioContainer.css'
 export default function PortfolioContainer() {
   return (
     <div className='port-container'>
-        <Ikigai />
+
         <Intro />
         <ProjectContainer />
         <NavBar />
