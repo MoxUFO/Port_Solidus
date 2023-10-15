46eb9bb106237cbf9590620442b104fdfaa997bb
MOX UFO <mox.ufo@gmail.com>
2023-10-15 17:03:54 -0400

    working on media queries

--- src/componenets/Intro.js
+++ src/componenets/Intro.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import IntroButtons from './IntroButtons'
-import '../assest/css/Intro.css'
+import React from "react";
+// import IntroButtons from './IntroButtons'
+import "../assest/css/Intro.css";
 
 export default function Intro() {
   return (
-    <div className='intro-card'>
+    <div className="intro-containter">
+      <div className="intro-text">
         <h2>NELSON FOSTER JR</h2>
         <div className='role'>
             <h1>FULL STACK</h1>
             <h1>DEVELOPER</h1>
         </div>
         <h3>Welcome to my projects portfolio!</h3>
-        <IntroButtons />
+      </div>
+      <div className="intro-btns"></div>
     </div>
-  )
+  );
 }
