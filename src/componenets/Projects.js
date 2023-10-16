dbf49f8e318e83b48f61a8f33c4e14677c369af1
MOX UFO <mox.ufo@gmail.com>
2023-10-15 23:55:10 -0400

    intro now adjust to media queries

--- src/componenets/Projects.js
+++ src/componenets/Projects.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import '../assest/css/Projects.css'
+import React from "react";
+import "../assest/css/Projects.css";
 
-export default function Projects({projects}) {
+export default function Projects({ projects }) {
   return (
     <div>
-        {projects.map((project, index)=> (
-          <div class="blog-slider">
+      {projects.map((project, index) => (
+        <div class="blog-slider">
           <div class="blog-slider__item swiper-slide">
-                  <div class="blog-slider__img">
-                      <img src={project.img} alt=""/>
-                  </div>
-                  <div class="blog-slider__content">
-				<div class="blog-slider__title">{project.title}</div>
-                <span class="blog-slider__code">Skills Used: {project.tools}</span>
-				<div class="blog-slider__text">{project.description}</div>
-				<a href={project.repo} class="blog-slider__button">Repository</a>
-                <a href={project.live} class="blog-slider__button">Live site</a>
-                  </div>
-              </div>
-      </div>
-        ))}
+            <div class="blog-slider__img">
+              <img src={project.img} alt="" />
+            </div>
+            <div class="blog-slider__content">
+              <div class="blog-slider__title">{project.title}</div>
+              <span class="blog-slider__code">
+                Skills Used: {project.tools}
+              </span>
+              <div class="blog-slider__text">{project.description}</div>
+              <a href={project.repo} class="blog-slider__button">
+                Repository
+              </a>
+              <a href={project.live} class="blog-slider__button">
+                Live site
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
