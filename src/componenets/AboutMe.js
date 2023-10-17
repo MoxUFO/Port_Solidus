65f3eb4265b037b5c50a1be8c189c8441d4db89f
MOX UFO <mox.ufo@gmail.com>
2023-10-17 15:37:06 -0400

    adjust about me layout

--- src/componenets/AboutMe.js
+++ src/componenets/AboutMe.js
@@ -1,43 +1,45 @@
 import React from "react";
 import "../assest/css/AboutMe.css";
+import node from '../assest/images/node.svg'
 
 export default function AboutMe() {
   return (
-    <div className="about-me-containter">
-      <div className="bio">
-        <div className="bio-img-holder">
-          <div className="bio-img"></div>
-        </div>
-        <div className="bio-text">
-          <p>
-            Greetings, I'm Nelson Foster Jr, a Full Stack Developer with a
-            foundation built at The Ohio State University. My journey into tech
-            has been a unique one, transitioning from a background in carpentry
-            to the world of code and design. As I initiate this new chapter, I
-            now serve as a Teaching Assistant in the very same program where I
-            began my journey into the arts of Front-End and Back-End coding.
-            Beyond coding, you'll often find me loggin time in the great
-            outdoors, camping alongside my wife, enjoying a meal over the open
-            fire. In my personal downtime, my passions include video games and
-            embarking on epic adventures in the world of Dungeons & Dragons.
-            These are the sparks that fuel my creativity and kindle my
-            curiosity.
-          </p>
-        </div>
-        <div className="bio-edu">
-          <h3>EDUCATION</h3>
-          <p>Certificate in Full Stack Coding</p>
-        </div>
+    <div id="About-Me" className="about-me-containter">
+      <div className="about-title">
+        <h1>ABOUT ME</h1>
       </div>
-      <div className="skills">
-        <div className="skills-header">
-          <h3>SKILLS</h3>
+      <div className="bio-content">
+        <div className="bio">
+          <div className="bio-img-holder">
+            <div className="bio-img"></div>
           </div>
+          <div className="bio-text " >
+            <h3>Who I am</h3>
+            <p >
+              I'm Nelson Foster Jr, a Full Stack Developer with roots in
+              carpentry, now transitioning to the coding world through The Ohio
+              State University's full stack bootcamp. Currently, I work as a
+              Teaching Assistant in the same program where my coding journey
+              began. Beyond coding, I have a deep love for the great outdoors
+              and often spend time camping with my wife, enjoying open-fire
+              meals. During my personal downtime, I'm passionate about video
+              games and embarking on epic adventures in the world of Dungeons &
+              Dragons. For further information about my projects or to get to
+              know me better, please do not hesitate to reach out.
+            </p>
+          </div>
+        </div>
+        <div className="skills">
+        <div className="skills-header">
+              <h3>SKILLS</h3>
+            </div>
           <div className="skills-icons">
+            <div>
+            </div>
 
           </div>
         </div>
       </div>
-    
+    </div>
   );
 }
