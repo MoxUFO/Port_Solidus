5f96ed6d8c7058bf04dd00ed11b3b2b6f8755da4
MOX UFO <mox.ufo@gmail.com>
2023-10-18 21:34:50 -0400

    add icons to the skills area of about me

--- src/componenets/AboutMe.js
+++ src/componenets/AboutMe.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "../assest/css/AboutMe.css";
-import node from '../assest/images/node.svg'
+import node from "../assest/images/node.svg";
+import MUI from "../assest/images/material-ui-1.svg";
+import Ract from "../assest/images/react.svg";
+import Mongo from "../assest/images/mongodb.svg";
+import Mysql from "../assest/images/mysql.svg";
+import Jquery from "../assest/images/jquery.svg";
+import JS from "../assest/images/javascript.svg";
+import Html from "../assest/images/html.svg";
+import HBs from "../assest/images/handlebars.svg";
+import Git from "../assest/images/github.svg";
+import Exp from "../assest/images/express.svg";
+import CSS from "../assest/images/css.svg";
 
 export default function AboutMe() {
   return (
@@ -13,9 +24,9 @@ export default function AboutMe() {
           <div className="bio-img-holder">
             <div className="bio-img"></div>
           </div>
-          <div className="bio-text " >
+          <div className="bio-text ">
             <h3>Who I am</h3>
-            <p >
+            <p>
               I'm Nelson Foster Jr, a Full Stack Developer with roots in
               carpentry, now transitioning to the coding world through The Ohio
               State University's full stack bootcamp. Currently, I work as a
@@ -30,14 +41,28 @@ export default function AboutMe() {
           </div>
         </div>
         <div className="skills">
-        <div className="skills-header">
-              <h3>SKILLS</h3>
-            </div>
+          <div className="skills-header">
+            <h3>SKILLS</h3>
+          </div>
           <div className="skills-icons">
-            <div>
-              
+            <div className="icon-holder">
+              <img className="icon" src={Html} alt="#" />
+              <img className="icon" src={CSS} alt="#" />
+              <img className="icon" src={JS} alt="#" />
+              <img className="icon" src={Jquery} alt="#" />
             </div>
-
+            <div className="icon-holder">
+              <img className="icon" src={MUI} alt="#" />
+              <img className="icon" src={Ract} alt="#" />
+              <img className="icon" src={node} alt="#" />
+              <img className="icon" src={Exp} alt="#" />
+            </div>
+            <div className="icon-holder">
+              <img className="icon" src={HBs} alt="#" />
+              <img className="icon" src={Mongo} alt="#" />
+              <img className="icon" src={Mysql} alt="#" />
+              <img className="icon" src={Git} alt="#" />
+            </div>
           </div>
         </div>
       </div>
