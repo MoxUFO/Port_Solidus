c5b11806ecaaf1cfbc4afb7523fdbabeaa0fcbc2
MOX UFO <mox.ufo@gmail.com>
2023-10-19 11:17:46 -0400

    added button to portfoilo source code into intro

--- src/componenets/Intro.js
+++ src/componenets/Intro.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import IntroButtons from './IntroButtons'
+import IntroButtons from "./IntroButtons";
 import "../assest/css/Intro.css";
 
 export default function Intro() {
@@ -7,13 +7,15 @@ export default function Intro() {
     <div className="intro-containter">
       <div className="intro-text">
         <h2>NELSON FOSTER JR</h2>
-        <div className='role'>
-            <h1>FULL STACK</h1>
-            <h1>DEVELOPER</h1>
+        <div className="role">
+          <h1>FULL STACK</h1>
+          <h1>DEVELOPER</h1>
         </div>
         <h3>Welcome to my projects portfolio!</h3>
       </div>
-      <div className="intro-btns"></div>
+      <div className="intro-btns">
+        <IntroButtons />
+      </div>
     </div>
   );
 }
--- src/componenets/IntroButtons.js
+++ src/componenets/IntroButtons.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import Button from '@mui/material/Button';
-import '../assest/css/IntroButtons.css'
+import React from "react";
+import Button from "@mui/material/Button";
+import "../assest/css/IntroButtons.css";
 
 export default function IntroButtons() {
-
-    let styles = {
-        "&.MuiButton-contained" : {
-            backgroundColor: '#201d20'
-        }
-    }
+  let styles = {
+    "&.MuiButton-contained": {
+      backgroundColor: "#201d20",
+      color: "#77835d",
+      fontSize: "16px",
+      fontWeight: 'bold'
+    },
+  };
 
   return (
-    <div className='btn-div'>
-         <Button className='intro-btn'sx={styles} color='inherit' variant="contained">Source Code</Button>
-         <Button className='intro-btn'sx={styles} color='inherit' variant="contained" >My GitHub</Button>
+    <div className="btn-div">
+      <Button
+      href="https://github.com/MoxUFO/Port_Solidus"
+        className="intro-btn"
+        sx={styles}
+        color="inherit"
+        variant="contained"
+      >
+        {" "}
+        Portfolio Source Code
+      </Button>
+      {/* <Button className='intro-btn'sx={styles} color='inherit' variant="contained" >My GitHub</Button> */}
     </div>
-  )
+  );
 }
